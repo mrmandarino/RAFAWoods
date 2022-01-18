6d5b3d413f3711ee1ecb6dfe62b0aa16f15ca26a
HelmerPSandoval <alonsospider_1998@hotmail.com>
2022-01-18 12:52:05 -0300

    migraciones+laravel breeze

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-//COmentario PRueba helmerS
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
