e5d59b39335392da0d18531f41c2d7273cde8c45
René Avendaño Zarate <samuel.benicio.delmar@gmail.com>
2022-01-20 15:04:19 -0400

    dependencias de bootstrap instaladas en public (bootstrap funcionando 100%)

---
+++ resources/views/ventas/portal_ventas.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+
+    <!-- CSS  --->
+    <title>Document</title>
+</head>
+<body>
+    
+    <h1>Hola Mundo</h1>
+    <!-- Example single danger button -->
+<div class="btn-group gap-2">
+    <button type="button" class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+      Wea
+    </button>
+    <div class="dropdown-menu">
+      <a class="dropdown-item" href="#">Action</a>
+      <a class="dropdown-item" href="#">Another action</a>
+      <a class="dropdown-item" href="#">Something else here</a>
+      <div class="dropdown-divider"></div>
+      <a class="dropdown-item" href="#">Separated link</a>
+    </div>
+</div>
+
+  <script src="js/bootstrap.bundle.min.js"></script>
+
+</body>
+</html>
