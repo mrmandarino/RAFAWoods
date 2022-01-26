8107525fb0fc359b59870111f910994414b0c90e
HelmerPSandoval <alonsospider_1998@hotmail.com>
2022-01-26 11:01:32 -0300

    Creacion rama INVENTARIO

--- resources/views/inventario/portal_inventario.blade.php
+++ resources/views/inventario/portal_inventario.blade.php
@@ -9,7 +9,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    Hola Ejecutivo!
+                    Hola Señor Ejecutivo!
                 </div>
             </div>
             <a href="{{route('admin_crear_usuario')}}" class="bg-ucn-color hover:bg-ucn-color focus:bg-green-900 | focus:outline-none border rounded-md text-white focus:text-white p-2 transition ease-in-out duration-150">
