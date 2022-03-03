6c404ce4703561f3bbf11e323173e7b9b7d44a0c
HelmerPSandoval <alonsospider_1998@hotmail.com>
2022-03-03 17:16:19 -0300

    Push respaldo para trabajar en modificador de aspect ratio de imagenes a 4:3

--- resources/views/catalogo/portal_catalogo.blade.php
+++ resources/views/catalogo/portal_catalogo.blade.php
@@ -377,7 +377,7 @@
                 <div class="col-12 col-md-6 col-lg-4">
                   <div class="card border-test shadow-sm ">
                       @if ($contador_aux <= 0)
-                      <a href="#"><img src="images\Imagen_no_disponible.svg.png" style="height:100%;width:100%" data-bs-toggle="modal" data-bs-target="#modal_detalle{{$producto->id}}"></a>
+                      <a href="#"><img src="images\imagen_no_disponible.png" style="height:100%;width:100%" data-bs-toggle="modal" data-bs-target="#modal_detalle{{$producto->id}}"></a>
                       @else
                         @foreach($imagenes as $imagen)
                         @if($imagen->imagenable_id == $producto->id)
@@ -439,7 +439,7 @@
                           <div class='carousel-inner'>
                             @if ($contador_aux <= 0)
                             <div class='carousel-item active'>
-                              <img class='img-size' src="{{ asset('images\Imagen_no_disponible.svg.png') }}" style="height:100%;width:100%" alt='First slide' />
+                              <img class='img-size' src="{{ asset('images\imagen_no_disponible_4_3_2.png') }}" style="height:100%;width:100%" alt='First slide' />
                             </div>
                             @else
                             @php
