caff330725c7eaef28b6cc3e91dfd59746cc3e29
Lelouch96 <luis-cubillos96@outlook.cl>
2022-03-15 17:05:35 -0300

    Seeders removidos

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,13 +19,13 @@ class DatabaseSeeder extends Seeder
         Storage::deleteDirectory('productos');
         Storage::makeDirectory('productos');
        
-
-        $this->call(InventarioSeeder::class);
         $this->call(UserSeeder::class);
-        $this->call(ProductoSeeder::class);
-        $this->call(VentaSeeder::class);
-        $this->call(ProveedorSeeder::class);
-        $this->call(Orden_compraSeeder::class);
+        // $this->call(InventarioSeeder::class);
+        
+        // $this->call(ProductoSeeder::class);
+        // $this->call(VentaSeeder::class);
+        // $this->call(ProveedorSeeder::class);
+        // $this->call(Orden_compraSeeder::class);
 
     }
 }
